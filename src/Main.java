ef7075e64ed4ee8a7ae8b96c9ee900a2a4e5560b
Wojciech Kęsek <wojtekesek@gmail.com>
2022-10-20 14:46:17 +0200

    add Locations class and clear code

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,11 @@
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static Map<Integer, Location> locations = new HashMap<>();
+    private static Locations locations = new Locations();
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Map <String, Integer> tempExit = new HashMap<>();
-        locations.put(0, new Location(0, "You are in front of computer",  tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 4);
-        tempExit.put("N", 5);
-        locations.put(1, new Location(1, "You are at the end of a road",  tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill",  tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building",  tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley",  tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest",  tempExit));
 
         Map<String, String> vocabulary = new HashMap<>();
         vocabulary.put("QUIT", "Q");
@@ -42,7 +13,6 @@ public class Main {
         vocabulary.put("SOUTH", "S");
         vocabulary.put("WEST", "W");
         vocabulary.put("EAST", "E");
-
 
         int loc = 1;
         while(true){
@@ -69,12 +39,12 @@ public class Main {
                 }
             }
 
-
             if(exits.containsKey(direction)){
                 loc = exits.get(direction);
             } else {
                 System.out.println("You cant go in that direction");
             }
         }
+        scan.close();
     }
 }
