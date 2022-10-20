14612a40d79beabc5b5ae84871bbaf338ac6ac4f
Wojciech Kęsek <wojtekesek@gmail.com>
2022-10-20 08:41:13 +0200

    upgrade user input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -18,21 +20,24 @@ public class Main {
         locations.get(1).addExit("E", 3);
         locations.get(1).addExit("S", 4);
         locations.get(1).addExit("N", 5);
-        locations.get(1).addExit("Q", 0);
 
         locations.get(2).addExit("N", 5);
-        locations.get(2).addExit("Q", 0);
 
         locations.get(3).addExit("W", 1);
-        locations.get(3).addExit("Q", 0);
 
         locations.get(4).addExit("N", 1);
         locations.get(4).addExit("W", 2);
-        locations.get(4).addExit("Q", 0);
 
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
-        locations.get(5).addExit("Q", 0);
+
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
 
         int loc = 1;
         while(true){
@@ -41,22 +46,30 @@ public class Main {
                 break;
             }
             Map<String, Integer> exits = locations.get(loc).getExits();
-            System.out.print("Avaliable exits are: ");
+            System.out.print("Available exits are: ");
             for(String exit: exits.keySet())
             {
                 System.out.print(exit + ", ");
             }
+            System.out.println();
+
             String direction = scan.nextLine().toUpperCase();
+            if(direction.length() > 1){
+                String[] parts = direction.split(" ");
+                for(String word: parts) {
+                    if (vocabulary.containsKey(word)){
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
+
             if(exits.containsKey(direction)){
                 loc = exits.get(direction);
             } else {
                 System.out.println("You cant go in that direction");
             }
-            loc = scan.nextInt();
-            if(!locations.containsKey(loc)){
-                System.out.println("You cant go there");
-            }
         }
-
     }
 }
